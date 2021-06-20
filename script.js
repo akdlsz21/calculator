894348b3c7c6f3b66ff91c4dd0f12840ac06af43
jihoon <akdlsz21@gmail.com>
2021-06-20 15:20:58 +0900

    d

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function operate(num1, operator, num2) {
 	} else if (operator == '-') {
 		return subtract(num1, num2);
 	} else if (operator == '*') {
-		return mulitply(num1, num2);
+		return multiply(num1, num2);
 	} else if (operator == '/') {
 		return divide(num1, num2);
 	}
@@ -32,7 +32,8 @@ let num2 = [];
 
 let button = document.querySelectorAll('.button-grid');
 let screen = document.querySelector('.screen');
-
+let clearBtn = document.querySelector('.clear-btn');
+let deleteBtn = document.querySelector('.delete-btn');
 // set a function to assign value to num1, operator, num2 in an order
 
 button.forEach((item) => {
@@ -57,10 +58,43 @@ function getNum1(value) {
 		value != '*'
 	) {
 		temp.push(parseInt(value));
-	} else if (value == '=') 
-	else {
+		screen.textContent = parseInt(temp.join(''));
+	} else if (value == '=') {
+		num1Int = parseInt(num1.join(''));
+		num2Int = parseInt(num2.join(''));
+		operate(num1Int, operator, num2Int);
+		num1 = [];
+		num2 = [];
+		first = true;
+	} else {
 		operator = value;
 		first = false;
 	}
 }
-console.log(num1);
+
+// set a function when clearbtn click, delete all memory
+clearBtn.addEventListener('click', () => {
+	reset();
+});
+
+function reset() {
+	num1 = [];
+	num2 = [];
+	screen.textContent = 'Cleared';
+}
+
+// set a function when deleteBtn click, delete the end of array number
+
+deleteBtn.addEventListener('click', () => {
+	deleteInt();
+});
+
+function deleteInt() {
+	if (first) {
+		num1.pop();
+		screen.textContent = parseInt(num1.join(''));
+	} else {
+		num2.pop();
+		screen.textContent = parseInt(num2.join(''));
+	}
+}
