3670807f85fd2f92505bb719c761f41d0c9dfe06
jihoon <akdlsz21@gmail.com>
2021-06-20 13:23:16 +0900

    .

--- script.js
+++ script.js
@@ -1,41 +1,65 @@
 function add(num1, num2) {
-	return num1 + num2;
+	screen.textContent = num1 + num2;
 }
 
 function subtract(num1, num2) {
-	return num1 - num2;
+	screen.textContent = num1 - num2;
 }
 
 function multiply(num1, num2) {
-	return num1 * num2;
+	screen.textContent = num1 * num2;
 }
 
 function divide(num1, num2) {
-	return num1 / num2;
+	screen.textContent = num1 / num2;
 }
 
-function operate(operator, num1, num2) {
-	return operator(num1, num2);
+function operate(num1, operator, num2) {
+	if (operator == '+') {
+		return add(num1, num2);
+	} else if (operator == '-') {
+		return subtract(num1, num2);
+	} else if (operator == '*') {
+		return mulitply(num1, num2);
+	} else if (operator == '/') {
+		return divide(num1, num2);
+	}
 }
 
-let firstOperand = '';
-let secondOperand = '';
+let num1 = [];
+let operator = '';
+let num2 = [];
 
 let button = document.querySelectorAll('.button-grid');
-button.forEach((element) => {
-	element.addEventListener('click', (tar) => {
-		calculate();
+let screen = document.querySelector('.screen');
+
+// set a function to assign value to num1, operator, num2 in an order
+
+button.forEach((item) => {
+	item.addEventListener('click', (event) => {
+		getNum1(event.target.value);
 	});
 });
 
-function calculate(firstOperand, operator, secondOperand) {
-	if (operator == '+') {
-		return firstOperand + secondOperand;
-	} else if (operator == '-') {
-		return firstOperand - secondOperand;
-	} else if (operator == '*') {
-		return firstOperand * secondOperand;
-	} else if (operator == '/') {
-		return firstOperand / secondOperand;
+function getNum1(value) {
+	if (
+		value != '=' ||
+		value != '+' ||
+		value != '-' ||
+		value != '/' ||
+		value != '*'
+	) {
+		num1 += value;
+		console.log(num1);
+	} else if (
+		value == '=' ||
+		value == '+' ||
+		value == '-' ||
+		value == '/' ||
+		value == '*'
+	) {
+		operator += value;
+		console.log(operator);
 	}
 }
+console.log(num1);
